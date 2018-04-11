8c5fc0926f9b69591aa92a86f461b9c54146be89
Kumar Bhargav <kumarbhrgv201@gmail.com>
2018-04-10 20:10:32 -0600

    updated CNN with increased accuracy

--- project-files/classify_images.py
+++ project-files/classify_images.py
@@ -1,7 +1,7 @@
 import keras
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
-from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Conv2D, MaxPooling2D,Activation
 
 from skimage.io import imread
 import os
@@ -36,24 +36,28 @@ x_test = images[1000:]
 y_test = images_attr[1000:]
 input_shape = (image.shape[0], image.shape[1],1)
 model = Sequential()
-model.add(Conv2D(50, kernel_size=(3, 3),
-                 activation='relu',
-                 input_shape=input_shape,name='Conv1'))
-model.add(MaxPooling2D(pool_size=(3, 3)))
-model.add(Conv2D(5, (3, 3), activation='relu',name='Conv2'))
-model.add(MaxPooling2D(pool_size=(3, 3)))
-model.add(Dropout(0.3))
+model.add(Conv2D(32, (3, 3), input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Flatten())
-model.add(Dense(1000, activation='relu'))
-model.add(Dropout(0.3))
-model.add(Dense(len(y_train[0]), activation='softmax'))
-optimizer= keras.optimizers.SGD()
-model.compile(loss=keras.losses.categorical_crossentropy,
-              optimizer=optimizer,
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(y_train.shape[1]))
+model.add(Activation('sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer='rmsprop',
               metrics=['accuracy'])
 print(model.summary())
 model.fit(x_train, y_train,
           epochs=10,
-          verbose=1,batch_size= 10)
-score = model.evaluate(x_train[:20], y_train[:20], batch_size=20)
+          verbose=1,batch_size= 20,validation_split=0.1)
+score = model.evaluate(x_train[:20], y_train[:20], batch_size=10)
 print(score)
